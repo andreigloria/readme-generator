13f6dd55b70bfdbc8539f1f64984c25613600e24
andreigloria@gmail.com <andreigloria@gmail.com>
2023-02-21 23:33:33 +0000

    modified index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 #!/usr/env node
 console.log('hello');
+
+const readMe = {
+    title: "ReadMe Generator"
+    description: "This is a README Generator using a command-line application"
+    installation: "N/A"
+    usage: "N/A"
+    license: "MIT License"
+    contributing: "N/A"
+    tests: "N/A"
+    questions: "N/A"
+};
+
+const 
